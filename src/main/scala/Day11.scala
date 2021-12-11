fe523608e0f3172ba68d104725ed6b62d8c7b719
Milan Bharanya <mbharanya@users.noreply.github.com>
2021-12-11 16:19:18 +0100

    Day 11 new

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -4,41 +4,72 @@ import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 
 object Day11 extends App {
-  type CoordMap = Map[(Int, Int), Int]
+  type CoordMap = Map[Coordinate, Int]
 
-  class Octopus(val x: Int, val y: Int, val maybeEnergy: Option[Int], val hasFlashed: Boolean) {
-    def tick(map: CoordMap): List[Octopus] = {
-      val newEnergy = maybeEnergy.map(_ + 1)
-      if (newEnergy.exists(_ > 9) && !this.hasFlashed) {
-        val nextToTick = for {
-          newX <- (x - 1) until (x + 1)
-          newY <- (y - 1) until (y + 1)
-        } yield new Octopus(newX, newY, map.lift(newX, newY), false)
-        new Octopus(x, y, newEnergy, true) +: nextToTick.filter(!_.isSame(this)).toList //add self in flashed state
-      } else {
-        List(new Octopus(x, y, newEnergy, false))
-      }
+  case class Coordinate(x: Int, y: Int) {
+    def neighbors = {
+      (for {
+        newX <- (x - 1) to (x + 1)
+        newY <- (y - 1) to (y + 1)
+      } yield Coordinate(newX, newY)).toSet - this
     }
-
-    def isSame(a: Octopus) = a.x == this.x && a.y == this.y
   }
 
-  class OctopusMap(map: CoordMap) {
-    def tickOctopi(queue: Queue[Octopus], alreadyCounted: List[Octopus]): List[Octopus] = {
-      val (octopus, newQueue) = queue.dequeue
-
-      val newValuesToQueue = octopus.tick(map).filter(_ match {
-        case octopus: Octopus => octopus.maybeEnergy.exists(_ > 0) && !alreadyCounted.contains(octopus)
-      })
-
-      val newNewQueue = newQueue.enqueueAll(newValuesToQueue)
-
-      if (!newNewQueue.isEmpty) {
-        tickOctopi(newNewQueue, newValuesToQueue ::: alreadyCounted)
-      } else {
-        alreadyCounted
-      }
-    }
-
+  def findFlashes(map: CoordMap, flashes: Set[Coordinate]): Set[Coordinate] = {
+    val next: Set[Coordinate] = flashes ++
+      flashes
+        .flatMap(_.neighbors)
+        .filter(candidate => map
+          .lift(candidate)
+          .exists(_ + candidate.neighbors.intersect(flashes).size > 9))
+    if (next == flashes) flashes else findFlashes(map, next)
   }
+
+
+  def step(state: CoordMap): CoordMap = {
+    val incremented = state.view.mapValues(_ + 1).toMap
+    val flashes = findFlashes(incremented, incremented.filter(_._2 > 9).keySet)
+    incremented.map {
+      case (coord, value) => (coord,
+        if (flashes.contains(coord)) 0
+        else value + coord.neighbors.count(flashes.contains))
+    }
+  }
+
+  def part1(input: List[String]) = {
+    val map: CoordMap = (for {
+      x <- (0 until input(0).size)
+      y <- (0 until input.size)
+    } yield Coordinate(x,y) -> input(y).split("")(x).toInt).toMap
+
+    val stepResults: LazyList[CoordMap] = LazyList.iterate(map)(step)
+
+    println(stepResults.take(101).map(_.values.count(_ == 0)).sum)
+    stepResults
+  }
+
+  def part2(input: List[String]) = {
+    val map: CoordMap = (for {
+      x <- (0 until input(0).size)
+      y <- (0 until input.size)
+    } yield Coordinate(x,y) -> input(y).split("")(x).toInt).toMap
+
+    val stepResults: LazyList[CoordMap] = LazyList.iterate(map)(step)
+
+    val indexWithAll0 =  stepResults
+      .zipWithIndex
+      .filter(
+        (l, i) => l.values.count(_ == 0) == map.size
+      ).head._2
+
+    println(
+      indexWithAll0
+    )
+    indexWithAll0
+  }
+
+  part1(Util.getFileLines(11))
+  part2(Util.getFileLines(11))
+
+
 }
--- src/test/scala/Day11Test.scala
+++ src/test/scala/Day11Test.scala
@@ -1,5 +1,6 @@
 package ch.bharanya
 
+import ch.bharanya.Day11.Coordinate
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
@@ -29,12 +30,23 @@ class Day11Test extends AnyFlatSpec with should.Matchers {
   }
 
 
-  "Day 11 Part 1" should "get step 1 correct" in {
-    val octopusMap = TestData.map.map({
-      case (x,y) -> value => new Day11.Octopus(x = x, y = y, maybeEnergy = Some(value))
-    })
-    val tickedOctopi = new Day11.OctopusMap(TestData.map).tickOctopi(Queue.from(octopusMap), Nil)
-    tickedOctopi
+  "Day 11 Part 1" should "get steps correct" in {
+    val results = Day11.part1(TestData.testData.toList)
+    results(0).get(Coordinate(0,0)) should be(Some(5))
+    results(1).get(Coordinate(0,0)) should be(Some(6))
+    results(2).get(Coordinate(0,0)) should be(Some(8))
+    results(10).get(Coordinate(0,0)) should be(Some(0))
+
+
+    results.take(3).map(_.values.count(_ == 0)).sum should be(35)
+
+
+    results.take(11).map(_.values.count(_ == 0)).sum should be(204)
   }
+
+  "Day 11 Part 2" should "get step when all is 0" in {
+    val results = Day11.part2(TestData.testData.toList) should be(195)
+  }
+
 
 }
