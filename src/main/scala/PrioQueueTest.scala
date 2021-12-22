e982a628715fc3abc5e1b8f94ca6389ecbbbee6f
MBharanya <mbharanya@users.noreply.github.com>
2021-12-22 14:40:01 +0100

    Bug in scala???

--- src/main/scala/PrioQueueTest.scala
+++ src/main/scala/PrioQueueTest.scala
@@ -9,18 +9,13 @@ object PrioQueueTest extends App{
 
   val pq = new scala.collection.mutable.PriorityQueue[Elem]()(MyOrdering)  ++ List(Elem(1,1), Elem(0,0), Elem(2,2))
 
-  val pq2 = pq.clone
 
-  pq2.find(x => x.priority == 0) match {
+  pq.find(x => x.priority == 0) match {
     case Some(elem: Elem) => elem.priority = 3
     case None => println("Not found")
   }
 
-  println(pq)
-  println(pq.dequeue())
-  println(pq.dequeue())
-  println(pq.dequeue())
-
+  val pq2 = pq.clone
 
   println(pq2)
   println(pq2.dequeue())
